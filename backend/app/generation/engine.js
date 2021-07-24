c206bfd5d17a06e8f9cc2f8c064411d59fe67c78
David Delgadillo <daviddel.731@gmail.com>
2021-07-23 18:44:09 -0700

    refactored backend files

---
+++ backend/app/generation/engine.js
@@ -0,0 +1,27 @@
+const Generation = require('.');
+
+class GenerationEngine {
+  constructor() {
+    this.generation = null;
+    this.timer = null;
+  }
+
+  start() {
+    this.buildNewGeneration();
+  }
+
+  stop() {
+    clearTimeout(this.timer)
+  }
+
+  buildNewGeneration() {
+    this.generation = new Generation();
+
+    console.log('New generation', this.generation);
+
+    this.timer = setTimeout(() => this.buildNewGeneration(), this.generation.expiration.getTime() - Date.now()
+    );
+  }
+}
+
+module.exports = GenerationEngine;
