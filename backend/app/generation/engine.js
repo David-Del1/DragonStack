3df7354a9b418bed983933d910cb494be4462067
David Delgadillo <daviddel.731@gmail.com>
2021-07-27 09:48:28 -0700

    added dragon id to dragon table

--- backend/app/generation/engine.js
+++ backend/app/generation/engine.js
@@ -1,4 +1,5 @@
-const Generation = require('.');
+const Generation = require('./index.js');
+const GenerationTable = require('./table.js');
 
 class GenerationEngine {
   constructor() {
@@ -15,12 +16,24 @@ class GenerationEngine {
   }
 
   buildNewGeneration() {
-    this.generation = new Generation();
+    const generation = new Generation();
 
-    console.log('New generation', this.generation);
+    GenerationTable.storeGeneration(generation)
+      .then(({ generationId }) => {
+        this.generation = generation;
 
-    this.timer = setTimeout(() => this.buildNewGeneration(), this.generation.expiration.getTime() - Date.now()
-    );
+        this.generation.generationId = generationId;
+
+        console.log('New generation', this.generation);
+
+        this.timer = setTimeout(
+          () => this.buildNewGeneration(), 
+          this.generation.expiration.getTime() - Date.now()
+        );
+      })
+      .catch(error => console.error(error));
+
+    
   }
 }
 
